a59ed779399987f6e66c6e81d69eef5ea6ddb5c1
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-17 20:19:35 +0200

    prep. for test cases. Parent class for functions and hooks ready to use.

---
+++ src/test/java/FeatureFiles/testCase_0101P.feature
@@ -0,0 +1,11 @@
+#1. The user opens the akakce.com site in the browser.
+#2. On the main page, click ""Create Account"" option.
+#3. The user fills the required information completely.
+#4. Click the ""Create Account"" button.")
+
+Feature: Akakce.com Account Creation
+  As a user
+  I want to create an account on akakce.com
+  So that i can access the site's features as a registered user.
+
+  Scenario: User successfully creates an account on akakce.com
---
+++ src/test/java/Pages/Parent.java
@@ -0,0 +1,51 @@
+package Pages;
+
+import Utilities.GWD;
+import io.cucumber.java.ja.前提;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Parent {
+    WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
+    public JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+
+    public void myClick(WebElement element) {
+
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+    }
+
+    public void mySendKeys(WebElement element, String text) {
+
+        wait.until(ExpectedConditions.visibilityOf(element));
+        scrollToElement(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void verifyContainsText(WebElement element, String text) {
+
+        wait.until(ExpectedConditions.textToBePresentInElement(element,text));
+        Assert.assertTrue(element.getText().toLowerCase().contains(text.toLowerCase()));
+    }
+
+    public void myJSClick(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        scrollToElement(element);
+        js.executeScript("arguments[0].click();", element);
+    }
+
+
+    public void scrollToElement(WebElement element) {
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+
+}
