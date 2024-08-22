f99982dfc40fd078c3b20e3be6b35e13ad9a946b
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-22 22:58:18 +0200

    step def for testCase0501

--- src/test/java/StepDefinitions/testCase0501.java
+++ src/test/java/StepDefinitions/testCase0501.java
@@ -1,19 +1,25 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 
 public class testCase0501 {
+    DialogContent dc = new DialogContent();
+
     @And("the user clicks on Delete My Account")
     public void theUserClicksOnDeleteMyAccount() {
-
+        dc.myJSClick(dc.deleteMyAccountButton);
     }
 
     @And("the user enters invalid password")
     public void theUserEntersInvalidPassword() {
+        dc.mySendKeys(dc.enterPasswordInput,"123qwe123");
     }
 
     @Then("An error message should be displayed")
     public void anErrorMessageShouldBeDisplayed() {
+        dc.verifyContainsText(dc.alertBox,"Mevcut şifrenizi doğru girdiğinizden emin olun.");
+
     }
 }
